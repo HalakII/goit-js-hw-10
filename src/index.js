7da932fbc594260522aae311eb9e60d621992f37
HalakII <1017imiia@gmail.com>
2023-11-01 14:33:11 +0200

    pink styles

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ breedSelect.addEventListener('change', onSelectBreed);
 
 const createInfoMarkup = breed => {
   console.log(breed[0].breeds[0]);
-  const markup = ` <div class="box-img"><img class="cat-picture" src="${breed[0].url}" alt="${breed[0].breeds[0].name}"></div><div class="box-text"><h2 class="cat-info-desc-title">${breed[0].breeds[0].name}</h2>
+  const markup = ` <div class="box-img"><img class="cat-picture" src="${breed[0].url}" alt="${breed[0].breeds[0].name}" width="1600" height="1000"></div><div class="box-text"><h2 class="cat-info-desc-title">${breed[0].breeds[0].name}</h2>
     <p class="cat-info-desc-desc">${breed[0].breeds[0].description}</p>
     <p class="cat-info-desc-temp"><b>Temperament:</b> ${breed[0].breeds[0].temperament}</p></div>`;
   divCatIfoEl.insertAdjacentHTML('beforeend', markup);
